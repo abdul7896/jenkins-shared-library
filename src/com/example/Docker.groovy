eca0fb9033ade789e6474bf562804cdeaafb2095
Abdul Syed <syed_abdul_basit@hotmail.com>
2022-09-22 23:10:36 +0100

    added class and function

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -4,22 +4,28 @@ package com.example
 
 class Docker implements Serializable {
 
-        def script
+    def script
 
-        Docker(script){
-            this.script = script
-        }
+    Docker(script) {
+        this.script = script
+    }
 
-        def buildDockerImage(String imageName){
-            script.echo 'building the docker image'
-            script.withCredentials([script.usernamePassword(
-                    credentialsId: '71ef5aa5-53bc-40e9-bdf6-4cfd27c1aa7e',
-                    passwordVariable: 'PASS',
-                    usernameVariable: 'USER'
-            )]) {
-                script.sh "docker build -t $imageName ."
-                script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
-                script.sh "docker push $imageName"
-            }
+    def buildDockerImage(String imageName) {
+        script.echo 'building the docker image'
+        script.sh "docker build -t $imageName ."
+    }
+
+    def dockerLogin() {
+        script.withCredentials([script.usernamePassword(
+                credentialsId: '71ef5aa5-53bc-40e9-bdf6-4cfd27c1aa7e',
+                passwordVariable: 'PASS',
+                usernameVariable: 'USER'
+        )]) {
+            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
         }
     }
+
+    def dockerPush(String imageName){
+        script.sh "docker push $imageName"
+    }
+}
